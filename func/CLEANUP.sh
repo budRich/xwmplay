2dad5bc5b8385fe864166fa330fb064c3bb028bf
budRich <robstenklippa@gmail.com>
2023-09-23 20:01:39 +0000

    [CHANGE] moved cleanup from main() to CLEANUP trap
    If we have a trap we should use it.
    Also cleaned up the cleanup by applying tips from #1 and use rm -f instead
    of testing for file before rm

--- func/CLEANUP.sh
+++ func/CLEANUP.sh
@@ -3,7 +3,11 @@
 trap 'CLEANUP' EXIT INT HUP
 
 CLEANUP() {
-  
+
   ((___t)) && >&2 echo "<<< $___cmd" "$(( (10#${EPOCHREALTIME//[!0-9]} - ___t) / 1000 ))ms" #bashbud
-  :
+  
+  rm -f "$tmp_xevent_script"
+  rm -f "$tmp_i3sock"
+
+  pkill -P $$
 }
